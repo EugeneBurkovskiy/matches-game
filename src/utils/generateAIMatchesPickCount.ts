348d04c525a0d2818c151517d9de92ad0b94784b
Eugene <gradam2001@gmail.com>
2023-06-24 21:55:02 +0300

    fix: add matches hover style, change AI logic

--- src/utils/generateAIMatchesPickCount.ts
+++ src/utils/generateAIMatchesPickCount.ts
@@ -4,18 +4,53 @@ const generateAIMatchesPickCount = (
   AIMatchesCount: number
 ) => {
   const evenLeftMatchesCount = leftMatchesCount % 2 === 0;
-  if (AIMatchesCount % 2 !== 0) {
-    return 1;
-  }
-  if (evenLeftMatchesCount) {
-    if (leftMatchesCount === maxMatchesPerTurn) {
-      return maxMatchesPerTurn;
-    } else if (leftMatchesCount < maxMatchesPerTurn) {
-      return leftMatchesCount;
+  const evenHandMatchesCount = AIMatchesCount % 2 === 0;
+
+  const findMaxPossibleCount = (
+    maxMatchesPerTurn: number,
+    leftMatchesCount: number,
+    flag: 'odd' | 'even'
+  ) => {
+    const possibleCounts = [...new Array(maxMatchesPerTurn)]
+      .map((_, i) => i + 1)
+      .filter((item) => {
+        if (flag === 'even') return item <= leftMatchesCount && item % 2 === 0;
+        else if (flag === 'odd') return item <= leftMatchesCount && item % 2 !== 0;
+      });
+
+    const maxPossibleOddCount = Math.max(...possibleCounts);
+
+    if (isFinite(maxPossibleOddCount)) {
+      return maxPossibleOddCount;
     }
-    return 2;
+
+    return 1;
+  };
+
+  const maxPossibleEvenCount = findMaxPossibleCount(maxMatchesPerTurn, leftMatchesCount, 'even');
+  const maxPossibleOddCount = findMaxPossibleCount(maxMatchesPerTurn, leftMatchesCount, 'odd');
+
+  if (evenLeftMatchesCount) {
+    if (evenHandMatchesCount) {
+      if (leftMatchesCount === maxMatchesPerTurn) {
+        return maxMatchesPerTurn;
+      } else {
+        return maxPossibleEvenCount;
+      }
+    } else {
+      return maxPossibleOddCount;
+    }
+  } else {
+    if (evenHandMatchesCount) {
+      return maxPossibleEvenCount;
+    } else {
+      if (leftMatchesCount === maxMatchesPerTurn + 1) {
+        return maxPossibleOddCount;
+      } else {
+        return maxPossibleOddCount;
+      }
+    }
   }
-  return 1;
 };
 
 export { generateAIMatchesPickCount };
